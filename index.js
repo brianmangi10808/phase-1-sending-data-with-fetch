c517a1ef04e2737a1e5f565f6c067ff73ebfc4c9
Your Name <brianmwangi10808@gmail.com>
2024-01-30 06:16:52 +0300

    uncompleted

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+
+
 // Add your code here
+function submitData(usersEmail, usersName) {
+    const formData = {
+        usersName: usersName,
+        usersEmail: usersEmail,
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object);
+            return object;
+        })
+        .catch(function (error) {
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+            return error;
+        });
+}
+
+
